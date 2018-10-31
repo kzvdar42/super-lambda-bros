6df5b6ccbc6e48f19cca36f9517e83e19ceeb9b9
Maksim Surkov <modbrin@live.com>
2018-11-01 02:42:48 +0300

    added sample project, dependencies still need to be included in stack.yaml

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
-module Main where
 
-import App
+-- | Visibility on the 2D plane.
+--   Uses an instance of Warnocks algorithm.
+--   TODO: animate the line segments, make them spin and move around so we can see
+--         that it's a dynamic visiblity algorithm -- not pre-computed.
+--         Draw lines in random shades of color depending on the index.
+--         Make a key to swap between rectangular and polar projections.
+--         Allow viewpoint to be set with the mouse.
+--
+--  TODO:  To start with just do brute force visibility by dividing field into cells
+--         and doing vis based on center point of cell.
+--
+
+import Interface
+import Draw
+import State
+import World
+import Graphics.Gloss.Interface.Pure.Game
 
 main :: IO ()
-main = run
+main
+ = do   world           <- initialWorld
+        let state       =  initialState world
+        
+        play   FullScreen
+               black 80 state
+               drawState handleInput stepState
