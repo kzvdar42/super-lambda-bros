5c56d1cd8383fa8dd03210c8f01dce775dab79cb
Denis Chernikov <d.chernikov@innopolis.ru>
2018-11-25 17:47:55 +0300

    Code structure improved; death of the player implemented

--- src/Update.hs
+++ src/Update.hs
@@ -10,14 +10,13 @@ import Lib
 -- | Check if the player's head collides with some block.
 -- And if is, run the `performCollisions`.
 checkCollision :: Game -> Game
-checkCollision game@(Game lvls player state) =
-  case takeElemFromMatrix (levelMap lvl) (x_close, y) of
-    Nothing -> case takeElemFromMatrix (levelMap lvl) (x_far, y) of
-      Nothing -> game
+checkCollision game@(Game _ curlvl player _) =
+  case takeElemFromMatrix (levelMap curlvl) (x_close, y) of
+    Nothing -> case takeElemFromMatrix (levelMap curlvl) (x_far, y) of
+      Nothing -> if pos_y < 0 then performCollisions [(Die, (x, y))] game else game
       Just tile -> performCollisions (map (\c -> (c, (x_far, y))) (typeOfCollision tile)) game
     Just tile -> performCollisions (map (\c -> (c, (x_close, y))) (typeOfCollision tile)) game
   where
-    lvl = lvls !! gameStateLvlNum state -- TODO: do this is a safe way.
     (MovingObject kind (pos_x, pos_y) _ _ _ _) = player
     (x, y) = mapPosToCoord (pos_x, pos_y + (snd (getSize kind)) + (tileSize - minObjSize) - thresh)  -- TODO: change when will generalize minObjSize
     (x_r, _) = mapPosToCoord (pos_x + (fst (getSize kind)), pos_y)
@@ -30,41 +29,43 @@ checkCollision game@(Game lvls player state) =
 -- Right now the implementation is fixed to the position of the player.
 performCollisions :: [(CollisionType, Coord)] -> Game -> Game
 performCollisions [] game = game
-performCollisions (c:cs) (Game lvls player state) =
+performCollisions (c:cs) (Game lvls curlvl player state) =
   performCollisions cs $ case c of
     (Delete, tile_pos) ->
-      (Game (updateLvlMap lvls lvlNum tile_pos Empty) player state)
+      (Game lvls (updtile tile_pos Empty) player state)
     (Spawn objKind (off_x, off_y), (tile_x, tile_y)) ->
       let
-        newlvls = updateElemInList lvls newlvl (fromIntegral lvlNum)
-        newlvl = Level
-          { levelMap = levelMap lvl
-          , levelInitPoint = levelInitPoint lvl
+        updlvl = Level
+          { levelMap = levelMap curlvl
+          , levelInitPoint = levelInitPoint curlvl
           , levelObjs = ((MovingObject objKind
               (mapCoordToPos (tile_x + off_x, tile_y + off_y))
               (1.0 * tileSize, 0.0) (0.0, 0.0) 0 0) : objects)
           }
       in
-      (Game newlvls player state)
+      (Game lvls updlvl player state)
     (Change tile, tile_pos) ->
-      (Game (updateLvlMap lvls lvlNum tile_pos tile) player state)
+      (Game lvls (updtile tile_pos tile) player state)
     (Bounce, _) ->
-      Game lvls (MovingObject kind pos
+      Game lvls curlvl (MovingObject kind pos
       (vel_x, -2 * minObjSize) (accel_x, 0.0) animC animD) state
-    (CollectCoin, _) -> Game lvls player (incrementCoins state)
+    (CollectCoin, _) -> Game lvls curlvl player (incrementCoins state)
+    (Die, _) ->
+      Game lvls
+      initlvl
+      (initPlayer (levelInitPoint initlvl))
+      state {gameStateHp = gameStateHp state - 1}
   where
-    lvl = lvls !! lvlNum  -- TODO: Do this is a safe way
-    objects = levelObjs lvl
+    objects = levelObjs curlvl
+    initlvl = lvls !! (gameStateLvlNum state)
     (MovingObject kind pos (vel_x, _) (accel_x, _) animC animD) = player
-    lvlNum = gameStateLvlNum state
-    incrementCoins (GameState hp coins l n_lvl ks)
-      | coins < 100 = GameState hp       (coins + 1)  l n_lvl ks
-      | otherwise   = GameState (hp + 1) (coins - 99) l n_lvl ks
-
--- -- | Apply gravity to the `MovingObject`.
--- applyGravity :: Float -> MovingObject -> MovingObject
--- applyGravity dt (MovingObject kind pos vel (accel_x, accel_y))
---   = MovingObject kind pos vel (accel_x, accel_y - g * dt)
+    updtile t_pos t =
+      curlvl {levelMap = updateElemInMatrix (levelMap curlvl) t_pos t}
+    incrementCoins gState
+      | coins < 100 = gState { gameStateCoins = coins + 1 }
+      | otherwise   = gState { gameStateHp = gameStateHp gState + 1
+                             , gameStateCoins = coins - 99 }
+      where coins = gameStateCoins gState
 
 -- | Apply gravity to the `MovingObject`.
 applyGravityAsVel :: Float -> MovingObject -> MovingObject
@@ -184,26 +185,23 @@ canMove lvl pos@(pos_x, pos_y) =
 
 -- | Physics of the game.
 updateGame :: (Int, Int) -> Float -> Game -> Game
-updateGame res dt (Game lvls player state) =
+updateGame res dt game@(Game lvls curlvl player state) =
   case gameStateNextLvlNum state of
-    Nothing -> checkCollision (Game updlvls upd_player state)
-    Just nextLevel -> Game updlvls upd_player
-      (state {gameStateLvlNum = nextLevel, gameStateNextLvlNum = Nothing})
+    Nothing -> checkCollision (Game lvls updlvl upd_player state)
+    Just nextLevel -> game -- TODO:  move to next level.
   where
-    lvlNum = gameStateLvlNum state
-    lvl = lvls !! lvlNum -- TODO: make this is a safe way.
-    lvlMap = levelMap lvl
-    updlvls = updateElemInList lvls (lvl { levelObjs = upd_objects }) (fromIntegral lvlNum)
+    lvlMap = levelMap curlvl
+    updlvl = curlvl { levelObjs = upd_objects }
     (MovingObject _ plr_pos _ _ _ _) = player
     upd_player =
       (( updatePlayer dt lvlMap
       . performActions lvlMap (S.toList (pressedKeys state))
       ) player)
-    upd_objects = updateObjects res dt lvlMap plr_pos (levelObjs lvl)
+    upd_objects = updateObjects res dt lvlMap plr_pos (levelObjs curlvl)
 
 -- | Update player state.
 updatePlayer :: Float -> LevelMap -> MovingObject -> MovingObject
-updatePlayer dt lvlMap = 
+updatePlayer dt lvlMap =
   updateAnimation dt lvlMap . tryMove dt lvlMap . applyFriction lvlMap . applyGravityAsVel dt
 
 -- | Update objects due the current position of player.
@@ -218,4 +216,4 @@ updateObjects res@(res_x, _) dt lvlMap plr_pos@(plr_pos_x, _) (obj:objs)
   where
     (MovingObject _ (pos_x, _) _ _ _ _) = obj
     leftBoundary = plr_pos_x - (fromIntegral res_x)
-    rightBoundary = plr_pos_x + (fromIntegral res_x)/2
+    rightBoundary = plr_pos_x + tileSize * 10-- (fromIntegral res_x)/4
